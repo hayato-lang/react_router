457cca5a9b1f6ad43d9da4a0a2f999905453becb
hayato-lang <83726781+hayato-lang@users.noreply.github.com>
2021-11-15 14:19:58 +0900

    ルート定義の分割

--- src/App.js
+++ src/App.js
@@ -1,9 +1,5 @@
-import { BrowserRouter, Link, Switch, Route } from "react-router-dom";
-import { Home } from "./Home";
-import { Page1 } from "./Page1";
-import { Page2 } from "./Page2";
-import { Page1DetailA } from "./Page1DetailA";
-import { Page1DetailB } from "./Page1DetailB";
+import { BrowserRouter, Link } from "react-router-dom";
+import { Router } from "./router/Router";
 import "./styles.css";
 
 export default function App(props) {
@@ -16,30 +12,7 @@ export default function App(props) {
         <br />
         <Link to="/Page2">Page2</Link>
       </div>
-      <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route
-          path="/Page1"
-          render={({ match: { url } }) => (
-            <Switch>
-              <Route exact path={url}>
-                <Page1 />
-              </Route>
-              <Route path={`${url}/DetailA`}>
-                <Page1DetailA />
-              </Route>
-              <Route path={`${url}/DetailB`}>
-                <Page1DetailB />
-              </Route>
-            </Switch>
-          )}
-        />
-        <Route path="/Page2">
-          <Page2 />
-        </Route>
-      </Switch>
+      <Router />
     </BrowserRouter>
   );
 }
