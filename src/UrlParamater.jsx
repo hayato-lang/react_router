f22e7ca901456eb22597ca9dd4957f4f8615320b
hayato-lang <83726781+hayato-lang@users.noreply.github.com>
2021-11-17 07:47:22 +0900

    useLocation

--- src/UrlParamater.jsx
+++ src/UrlParamater.jsx
@@ -1,15 +1,13 @@
-import { useParams, useRocation } from "react-router-dom";
+import { useLocation, useParams } from "react-router-dom";
 
 export const UrlParamater = () => {
   const { id } = useParams();
-  const { search } = useRocation();
-  const query = new URLSearchParams(search);
-
+  const location = useLocation();
+  console.log(location);
   return (
     <div>
       <h1>UrlParamaterです。</h1>
       <p>パラメーターは{id}です</p>
-      <p>クエリパラメーターは{query}です</p>
     </div>
   );
 };
