1ef111ef62df8752056f836ccbdfc052bb788463
hayato-lang <83726781+hayato-lang@users.noreply.github.com>
2021-11-17 09:11:03 +0900

    Linkを使わないページ遷移

--- src/Page1.jsx
+++ src/Page1.jsx
@@ -1,13 +1,17 @@
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 
 export const Page1 = () => {
+  const history = useHistory();
   const arr = [...Array(100).keys()];
+  const onClickDetailA = () => history.push("/Page1/DetailA");
   return (
     <div>
       <h1>Page1です</h1>
       <Link to={{ pathname: "/Page1/DetailA", state: arr }}>DetailA</Link>
       <br />
       <Link to="/Page1/DetailB">DetailB</Link>
+      <br />
+      <button onClick={onClickDetailA}>DetailA</button>
     </div>
   );
 };
--- src/Page1DetailA.jsx
+++ src/Page1DetailA.jsx
@@ -1,11 +1,15 @@
-import { useLocation } from "react-router-dom";
+import { useHistory, useLocation } from "react-router-dom";
 
 export const Page1DetailA = () => {
   const { state } = useLocation();
+  const history = useHistory();
+  const onClickPage1 = () => history.goBack();
   console.log(state);
   return (
     <div>
       <h1>Page1DetailAです</h1>
+      <br />
+      <button onClick={onClickPage1}>戻る</button>
     </div>
   );
 };
